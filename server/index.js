ff8cc2f6ccfcc95371d2d8a1a54a0d67e74a3e1e
Matthew Mccarthy <matthewmc96@gmail.com>
2020-11-04 21:20:26 -0600

    first commmit

---
+++ server/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
